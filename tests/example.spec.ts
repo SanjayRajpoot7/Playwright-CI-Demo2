40ab4fbc7661b60dea34b03c9ec1bfff25f4636a
Rajput <sanjay.a.rajput@capgemini.com>
2025-11-13 14:53:45 +0530

    Resolved the previous issue

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('homepage has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwrighting.../);
+  await expect(page).toHaveTitle(/Playwright/);
 
   
 });
